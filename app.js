199adc9cb0c46e12b61c2d47dc8c299979642105
hyobin <hbaloha@naver.com>
2024-04-08 18:48:44 +0900

    Paths

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath(); //새 경로, 이전 경로와 단절
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = 'red';
+ctx.fill();
