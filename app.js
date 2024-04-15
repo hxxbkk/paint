7f94b270899615f211afefd579e4f222f69adc37
hyobin <hbaloha@naver.com>
2024-04-15 19:25:04 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-  '#ffeaa7',
-  '#fab1a0',
-  '#55efc4',
-  '#81ecec',
-  '#a29bfe',
-  '#fd79a8',
-  '#d63031',
-  '#e17055',
-];
-
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener('mousemove', onClick);
+function startPainting() {
+  isPainting = true;
+}
+function canclePainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', canclePainting);
+canvas.addEventListener('mouseleave', canclePainting);
